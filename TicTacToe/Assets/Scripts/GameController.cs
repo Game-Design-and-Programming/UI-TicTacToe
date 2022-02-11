1d092836ab1fe20405e0f24d2d7600da7ce7e048
Douglas Urner <4495780+DouglasUrner@users.noreply.github.com>
2022-02-10 17:45:38 -0800

    Implement basic game control.

--- TicTacToe/Assets/Scripts/GameController.cs
+++ TicTacToe/Assets/Scripts/GameController.cs
@@ -6,4 +6,27 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public Text[] buttonList;
+
+    private void Awake()
+    {
+        SetGameControllerReferenceOnButtons();
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return "?";
+    }
+
+    public void EndTurn()
+    {
+        Debug.Log("EndTurn is not implemented");
+    }
 }
